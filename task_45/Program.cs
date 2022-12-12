54c1b1ba6b70622efd4d7359296dcd9cb13c0cbe
Dmitry Poddubnov <poddubnovd@yandex.ru>
2022-12-13 02:14:35 +0300

    Solution Task45

---
+++ task_45/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write("{0}", arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write("   ");
+        }
+    }
+    Console.WriteLine(" ]");
+}
+
+void FillArray(int[] arrayToFill, int diviation)
+{
+    for (int i = 0; i < arrayToFill.Length; i++)
+        arrayToFill[i] = new Random().Next(-diviation, diviation + 1);
+}
+
+void CopyArray(int[] arrayToCopy, int[] arrayToFill)
+{
+    for (int i = 0; i < arrayToCopy.Length; i++)
+        arrayToFill[i] = arrayToCopy[i];
+}
+
+
+int arraySize = 0;
+
+arraySize = new Random().Next(5, 15);
+int[] firstArray = new int[arraySize];
+int[] secondArray = new int[arraySize];
+FillArray(firstArray, 15);
+CopyArray(firstArray, secondArray);
+Console.Clear();
+
+Console.WriteLine("Первый массив:");
+PrintArray(firstArray);
+Console.WriteLine();
+Console.WriteLine("Копия первого:");
+PrintArray(secondArray);
+Console.ResetColor();
